86a492df06b38c1c8ae960b3a4b5d97e2d6207ab
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-10-27 14:42:25 -0400

    Pi Day

---
+++ implementation.py
@@ -0,0 +1,6 @@
+# Store the months of the year as strings. Determine the month in the data structure in which National Pi Day exists
+#  and print that month to the console. 
+# HINT: The number for Pi, when converted to an Integer, is related to the stored location of the correct month.
+
+months_in_year = ('January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from implementation import months_in_year
+
+march = months_in_year[2]
+print(f"Pi day is the 14th of {march}")
