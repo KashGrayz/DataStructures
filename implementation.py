ca0385998e86e8e5e6ad20c09e909b94e7967328
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-10-27 18:06:19 -0400

    working on iterating dictionary in list

--- implementation.py
+++ implementation.py
@@ -30,3 +30,17 @@ user_profile = {
 }
 
 
+# Use a list to store the dictionary of your immediate family members, 
+# with each index of the list storing its own dictionary. 
+# Dictionary should contain the following keys:
+# First name
+# Last name
+# Relation to you
+
+# Once you have stored the List of Dictionary items, write a function/method that will 
+# iterate over the List and print off the First Name and Relation of each person in the List.
+
+close_family = [  {'f_name': 'Bruce', 'l_name': 'Wayne', 'r_lation': 'Batman'},
+                  {'f_name': 'Kal', 'l_name': 'El', 'r_lation': 'Superman'},
+                  {'f_name': 'John', 'l_name': 'Stewart', 'r_lation': 'Green Lantern'},
+                  {'f_name': 'Katar', 'l_name': 'Hol', 'r_lation': 'Hawkman'}]
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+    #  DATA STRUCTURES  #
+
+#  // Task I \\
+
 #Store the months of the year as strings. Determine the month in the data structure in which National Pi Day exists
 #  and print that month to the console. 
 # HINT: The number for Pi, when converted to an Integer, is related to the stored location of the correct month.
 
-from implementation import months_in_year
-from implementation import fav_fruits
-from implementation import user_profile
+# from implementation import months_in_year
+# from implementation import fav_fruits
+# from implementation import user_profile
+from implementation import close_family
 # march = months_in_year[2]
 # print(f"Pi day is the 14th of {march}")
 
@@ -19,14 +24,23 @@ from implementation import user_profile
 #     print(pulp) 
 
 
-# Store information about a user profile. Use literal string interpolation to print the user’s profile information to the console. 
+# Store information about a user profile. 
+# Use literal string interpolation to print the user’s profile information to the console. 
 # The profile should consist of the following information:
 # First Name
 # Last Name
 # Email Address
 # Phone Number
 
-print(f'''My name is {user_profile["first_name"]} {user_profile["last_name"]}, 
-you can contact me by {user_profile["email_addy"]} 
-or by phone {user_profile["phone_number"]}.''')
+# print(f'''My name is {user_profile["first_name"]} {user_profile["last_name"]}, #interpolation for user profile
+# you can contact me by {user_profile["email_addy"]} 
+# or by phone {user_profile["phone_number"]}.''')
 
+#  // Task II \\
+
+#family =close_family(dict)
+
+for dict_item in close_family:
+    for key in dict_item:
+        print(dict_item[key])
+    
