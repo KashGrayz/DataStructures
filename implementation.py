0030011d46f5530124ec4f0edce5286aebb58557
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-10-27 15:03:28 -0400

    Implementing set to database

--- implementation.py
+++ implementation.py
@@ -2,5 +2,14 @@
 #  and print that month to the console. 
 # HINT: The number for Pi, when converted to an Integer, is related to the stored location of the correct month.
 
+#Months of the year tuple
 months_in_year = ('January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+
+# Store five fruits or vegetables.
+# Add two of your favorite fruits and two of your favorite vegetables to the collection.
+# Iterate over the collection and print each one to the console. 
+
+#Favorite fruits or veggies set
+
+fav_fruits = {'Mango', 'Pineapple', 'Guava', 'Kiwi', 'Blueberry'}
 
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from implementation import months_in_year
+#Store the months of the year as strings. Determine the month in the data structure in which National Pi Day exists
+#  and print that month to the console. 
+# HINT: The number for Pi, when converted to an Integer, is related to the stored location of the correct month.
 
-march = months_in_year[2]
-print(f"Pi day is the 14th of {march}")
+from implementation import months_in_year
+from implementation import fav_fruits
+
+# march = months_in_year[2]
+# print(f"Pi day is the 14th of {march}")
+
+
+# Store five fruits or vegetables.
+# Add two of your favorite fruits and two of your favorite vegetables to the collection.
+# Iterate over the collection and print each one to the console. 
+
+for seeds in fav_fruits:
+    print(seeds)
+fav_fruits.update('Coconut','Passionfruit','Tomato','Cucumber')
+for pulp in fav_fruits:
+    print(pulp)
